524150d134c2a90de9b4ec77605aa080c26c705e
matthew <matthewwolak@gmail.com>
2018-05-10 13:49:18 -0500

    cleanup or update import of Matrix

--- R/mkModMats.R
+++ R/mkModMats.R
@@ -30,7 +30,6 @@
 ################################################################################
 #' @describeIn gremlinR Generates model matrices. 
 #' @export
-#' @import Matrix
 #' @importFrom stats na.pass model.response model.matrix lm reformulate na.omit
 mkModMats <- function(formula, random = NULL, rcov = ~ units,
 		data = NULL, subset = NULL,
