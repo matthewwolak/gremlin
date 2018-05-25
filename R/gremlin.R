e55c01f400def6731d11769888eeb6c2c545c4e1
matthew <matthewwolak@gmail.com>
2018-05-25 15:34:08 -0500

    re-do recent commits that missed either `gremlin()` or `gremlinR()` in @Csparse-univariate
      - @Csparse-univariate copied `gremlinR()` into `gremlin()`
      - Some of last commits pushed from @devel went to the code in either `gremlin()` or `gremlinR()` because in same file

--- R/gremlin.R
+++ R/gremlin.R
@@ -225,6 +225,12 @@ gremlin <- function(formula, random = NULL, rcov = ~ units,
     cctol <- c(5*10^-4, 10^-8, 10^-3, NULL) # [1] for AI, alternatively 10^-5 (for EM)
   } else cctol <- eval(mc$cctol)
 #TODO check on validity of inputted algorithms (format and matching to actual ones)
+## Otherwise, get obscure warning about not finding `thetaout`
+## Something like the following line, but implement partial matching
+  if(!all(unique(algit) %in% c("EM", "AI", "bobyqa", "NR"))){  #TODO Update choices of algorithm if add/subtract any
+    stop(cat("Algorithms:", unique(algit)[which(!unique(algit) %in% c("EM", "AI", "bobyqa", "NR"))],
+      "not a valid choice. Please check values given to the `algit` argument\n"))
+  }
   if(is.null(mc$algit)) algit <- c(rep("EM", 2), rep("AI", max(0, maxit-2))) else algit <- eval(mc$algit)
   if(length(algit) == 1 && algit %in% c("EM", "AI")) algit <- rep(algit, maxit)
   if(is.null(mc$ezero)) ezero <- 1e-8 else ezero <- eval(mc$ezero)
@@ -757,7 +763,7 @@ i <- 1#  for(i in 1:nrow(itMat)){
         # AI only
 #        cc[4] <- -1 * c(crossprod(dLdtheta, H) %*% dLdtheta)
       }
-    }
+    } else cc[1] <- FALSE  #<-- ensures one of the EM/AI/etc algorithms used if i==1
 
 
 
@@ -830,7 +836,7 @@ if(nrow(theta[[thetaR]]) != 1){
     }
     units(itTime) <- "secs"
     itMat[i, ncol(itMat)] <- round(itTime, 1)
-    if(all(cc)){
+    if(all(cc, na.rm = TRUE)){
       cat("REML converged\n\n")
       break
     }
