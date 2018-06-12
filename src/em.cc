0aaeb9b0492a6f259a56d790f02b357633cf6f0c
matthew <matthewwolak@gmail.com>
2018-06-12 15:52:44 -0500

    further tweaks to EM algorithm in c++ not finished

--- src/em.cc
+++ src/em.cc
@@ -12,22 +12,81 @@ XXX eqn. 2.44 is the score/gradient! for a varcomp
 */
 
 /* theta overwritten with solution */
-csi cs_em(const cs *BLUXs, double *theta, csi nG, csi *rfxlvls, csi nb){
+csi cs_em(const cs *BLUXs, const double *theta,
+	csi nG, csi *rfxlvls, csi nb, csi *ndgeninv,
+	cs **geninv, cs *Cinv
+){
 
-  cs      *tmp_sln;
-  double  *Bx;
-  csi     g, k, si, qi, ei;
-  if (!CS_CSC (BLUXs) || !nb || !theta) return (0);    // check arguments
-  Bx = BLUXs->x;
- 
-  si = nb;
+  double  *Bx, *tmp_sln, roqi, o;
+  cs      *preTR, *preTRprod;
+  csi     g, i, j, k, m, si, qi, ei;
+  if(!CS_CSC (BLUXs) || !nb || !theta) return (0);    // check arguments
+  Bx = tmp_sln = BLUXs->x;
+
+  // Find location of rfxlvls with maximum number of levels
+//FIXME: DELETE DELETE if don't end up using!!!
+  m = 0;
+  if(nG > 0){
+    if(nG > 1){
+      for(g = 1; g < nG; g++) m = (rfxlvls[g] < rfxlvls[m]) ? m : g;
+    }
+  }
+//XXX  DELETE??   XXX
+
+  si = nb; o = 0.0;
   for(g = 0; g < nG; g++){
     qi = rfxlvls[g];
+    roqi = theta[nG];                                 //   residual var / qi
     ei = si - 1 + qi;
-//    tmp_sln = spalloc(qi, 1, qi, true, false);
-//    for(k = 0; k < qi; k++){
-//      tmp_sln
-  }
+
+
+    // crossproduct of BLUXs[si:ei] with geninv (t(BLUXs) %*% geninv)
+    //// Diagonal geninv first
+    if(ndgeninv[g] == 0){
+      for(k = si; k <= ei; k++){
+        tmp_sln[k] *= qi;
+      }
+    } else{
+      //// Non-diagonal generalized inverses
+      ////// Zero gth range
+      for(k = si; k <= ei; k++) tmp_sln[k] = 0.0;
+      ////// Column-by-column of geninv
+      for(k = 0; k < qi; k++){
+        ////// Each row (i) in col k of geninv
+        for(i = geninv[g]->p[k]; i < geninv[g]->p[k+1]; i++){
+          ////// Each element in Bx
+          for(j = si; j <= ei; j++){
+            tmp_sln[j] += Bx[j] * geninv[g]->x[i];
+          }  // end for j (elements in BLUXs)
+        }  // end for i (rows of geninv column k)
+      }  // end for k (columns of geninv)
+    }  // end if/else whether geninv is diagonal
+
+    // Above post-multiplied by BLUXs (... %*% BLUXs)
+    for(k = si; k <= ei; k++){
+      for(i = si; i <= ei; i++){
+        o += tmp_sln[k] * Bx[i];
+      }  // end for i (rows of BLUXs)
+    }  // end for k (cols of tmp_sln)
+
+
+    // ... + tr(geninv[g] %*% Cinv[si:ei, si:ei]) * residual variance / qi
+    preTR = cs_spalloc(qi, qi, Cinv->p[ei+1] - Cinv->p[si], true, false);
+      i = 0;
+      for(k = Cinv->p[si]; k < Cinv->p[ei+1]; k++){
+        preTR->i[i] = Cinv->i[k];
+        preTR->x[i] = Cinv->x[k];
+        i++;
+      }
+      for(k = 0; k <= qi; k++){
+        preTR->p[k] = Cinv->p[si+k];
+      }
+    preTRprod = cs_multiply(geninv[g], preTR);// * roqi
+
+    si = ei + 1;
+  }  // end for g in nG
+
+cs_spfree(preTR); cs_spfree(preTRprod);
 
   return(1);
 // use  cs_idone() to finish and return?
