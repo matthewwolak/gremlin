fe7e6f96078bec65a5765cc10d42f78a36c0c5b8
matthew <matthewwolak@gmail.com>
2018-10-02 22:46:39 -0500

    remove all cpp/c code - pure R implementation now

--- src/init.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h> // for NULL
-#include <R_ext/Rdynload.h>
-
-void R_init_gremlin(DllInfo *dll)
-{
-    R_registerRoutines(dll, NULL, NULL, NULL, NULL);
-    R_useDynamicSymbols(dll, TRUE);
-}
-
