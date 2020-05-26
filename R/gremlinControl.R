6945dbc965c2b7519efe1bc3666d7d2906b800b8
matthewwolak <matthewwolak@gmail.com>
2020-05-26 09:28:44 -0500

    add an upper limit to restrict maximum values to stay between

--- R/gremlinControl.R
+++ R/gremlinControl.R
@@ -6,6 +6,8 @@
 #' @param cctol Convergence criteria tolerances. TODO
 #' @param ezero Effective zero to be used, values less than this number are
 #'   treated as zero and fixed to this value.
+#' @param einf Effective infinite value to be used, values are limited to a
+#'   to this variable as a maximum.
 #' @param step A \code{numeric} value for scaling the proposed parameter updates.
 #' @param lambda A \code{logical} indicating whether a residual variance should
 #'   be factored out of the mixed model equations.
@@ -24,12 +26,12 @@
 #'
 #' @export
 gremlinControl <- function(cctol = c(5e-4, 1e-8, 1e-3, NULL),
-	ezero = 1e-8, step = 0.3, lambda = TRUE,
+	ezero = 1e-8, einf = 1e30, step = 0.3, lambda = TRUE,
 	algorithm = NULL, algArgs = list()){
 
   stopifnot(is.list(algArgs))
 
- return(structure(list(cctol = cctol, ezero = ezero, step = step,
+ return(structure(list(cctol = cctol, ezero = ezero, einf = einf, step = step,
 		lambda = lambda,
 		algorithm = algorithm, algArgs = algArgs),
 	class = c("gremlinControl")))
